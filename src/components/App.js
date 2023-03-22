da19e9679f574af89629d00374a8e52bf2d826b1
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2023-03-21 23:00:38 -0300

    Add initial elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import Button from './Button'
 
 function App() {
-  return (<div>
+  return (
+  <div>
     Hello world this is my first app
+    <h1>Welcome to my app</h1>
+    <br />
+    <Button />
   </div>)
 }
 
